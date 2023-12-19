72a6065f2f9ee1648bd7bcd07a3ac0a211d712b9
MatiasSaig <saig.matias@gmail.com>
2023-12-19 03:43:37 -0300

    entrega nr2 finalizada

--- src/components/Pages/RouteNotFound.jsx
+++ src/components/Pages/RouteNotFound.jsx
@@ -1,12 +1,20 @@
 import { Link } from "react-router-dom";
 
-function RouteNotFound(  ) {
-    return (
-        <>
-        <Link to="/" className="uppercase text-slate-100 text-3xl text-center px-10 py-5 font-bold my-5 bg-sky-600 hover:bg-sky-800">Volver al inicio</Link>
-<img src="../../../public/img/Error/404.webp" className=" opacity-90" alt="error 404"/>
-
-        </>
+function RouteNotFound() {
+  return (
+    <>
+      <Link
+        to="/"
+        className="uppercase text-slate-100 text-3xl text-center px-10 py-5 font-bold my-5 bg-sky-600 hover:bg-sky-800"
+      >
+        Volver al inicio
+      </Link>
+      <img
+        src="../../../public/img/Error/404.webp"
+        className=" opacity-90"
+        alt="error 404"
+      />
+    </>
   );
 }
 
