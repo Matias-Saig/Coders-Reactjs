b60de3530775eba3188002adc8f9a8e150c621b2
matias-saig <saig.matias@gmail.com>
2023-12-19 21:35:08 -0300

    actualización de estilos

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,17 +1,20 @@
 function ItemDetail({ item }) {
   return (
     <div className="w-3/4 py-3 flex flex-col items-center gap-5">
-      <h3 className="text-5xl text-center font-bold font-serif text-teal-800 bg-emerald-100 py-5 w-full">
+      <h3 className="text-5xl text-center font-bold font-serif text-teal-800 my-4 py-1 w-full border-solid border-b-2 border-slate-200">
         {item.title}
       </h3>
+      <div className="flex gap-5 justify-center w-4/5 items-center">
       <img
-        className="w-full object-cover h-80"
+        className="object-contain h-64 border-solid border-4 border-slate-300"
         src={item.img}
         alt={item.title}
       />
 
       <p className="text-lg">{item.description}</p>
-      <p className="text-3xl mt-7 font-bold text-emerald-700">
+      </div>
+
+      <p className="text-3xl mt-5 pt-7 font-bold text-emerald-700 border-solid border-t-2 border-slate-200 w-full text-center">
         Precio: ${item.price}
       </p>
       <p className="text-xl mt-3 font-bold text-stone-500">
