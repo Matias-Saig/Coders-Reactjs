2f8bc0ce111dfb7ccb830df7b8425a5107b50db4
MatiasSaig <saig.matias@gmail.com>
2024-01-18 17:29:34 -0300

    correcciones

--- src/components/Header/Logo/Logo.jsx
+++ src/components/Header/Logo/Logo.jsx
@@ -7,14 +7,14 @@ function Logo() {
         to="/"
         className="bg-gradient-to-b from-emerald-900 to-green-950 flex justify-center py-4"
       >
-        <img src="/public/img/Logo/logo-izq.png" alt="logo-izq" />
+        <img src="public/img/Logo/logo-izq.png" alt="logo-izq" />
 
         <h1 className="font-sans uppercase font-bold text-stone-100 text-center text-4xl tracking-wider">
           Menta peperina
           <span className="block text-3xl text-stone-400 opacity-50">Vivero urbano</span>
         </h1>
 
-        <img src="/public/img/Logo/logo-dre.png" alt="logo-dre" />
+        <img src="public/img/Logo/logo-dre.png" alt="logo-dre" />
       </NavLink>
     </>
   );
